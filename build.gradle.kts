41528788eace0148977f05261fc54a538a71d063
Vadim Vetrov <vetrovvd@gmail.com>
2023-06-28 16:42:52 +0300

    Migrate from maven to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "org.amfibot.discord"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val jdaVersion = "5.0.0-beta.11"
+dependencies {
+    implementation("net.dv8tion:JDA:$jdaVersion")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
