62d7079758c1d4fef6637602d16186cc976b29c3
Vadim Vetrov <vetrovvd@gmail.com>
2023-06-28 17:21:06 +0300

    Fixed exception on tests: token unenforced

--- src/test/kotlin/org/amfibot/discord/bot/helpers.kt
+++ src/test/kotlin/org/amfibot/discord/bot/helpers.kt
@@ -5,11 +5,14 @@ import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.utils.data.DataObject
 
-val emptyJda = JDABuilder.createDefault(null).build()
+/**
+ * May be used to fill getJDA methods. Fails when the method executed
+ */
+fun emptyJda(): JDA {return JDABuilder.createDefault(null).build()}
 
 val emptyEvent = object : GenericEvent {
     override fun getJDA(): JDA {
-        return emptyJda
+        return emptyJda()
     }
 
     override fun getResponseNumber(): Long {
