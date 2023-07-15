139035750f931340d53f10318dcc109a8460d8cf
Vadim Vetrov <vetrovvd@gmail.com>
2023-07-15 21:50:58 +0300

    Logging to the log channel

--- src/main/kotlin/org/amfibot/discord/bot/listeners/logging/events/MessageCreate.kt
+++ src/main/kotlin/org/amfibot/discord/bot/listeners/logging/events/MessageCreate.kt
@@ -1,5 +1,6 @@
 package org.amfibot.discord.bot.listeners.logging.events
 
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import org.amfibot.discord.bot.guild.Guild
@@ -10,11 +11,16 @@ object MessageCreate : LogsEventListener {
     override val logType: LogType = LogType.MESSAGE_CREATE
     override val eventClass: Class<MessageReceivedEvent> = MessageReceivedEvent::class.java
     override fun onEvent(event: GenericEvent, botGuild: Guild) {
-        if (event is MessageReceivedEvent) {
-            println(
-                "A message has been create by user ${event.author.name} with contents\n" +
+        if (event is MessageReceivedEvent && event.author.id != event.jda.selfUser.id) {
+            val message =
+                "A message has been create by user <@${event.author.id}> with contents\n" +
                         event.message.contentRaw
-            )
+
+            val channel =
+                (event.guild.channels.find { it.id == botGuild.generalModule.log.channel }
+                        as? MessageChannel?) ?: return
+
+            channel.sendMessage(message).queue()
         }
     }
 }
