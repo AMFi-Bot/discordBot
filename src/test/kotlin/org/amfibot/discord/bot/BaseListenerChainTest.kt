d1410b2d133069296afe46ce5824f4ead77997e2
Vadim Vetrov <vetrovvd@gmail.com>
2023-07-15 22:46:18 +0300

    fixed listener chain test

--- src/test/kotlin/org/amfibot/discord/bot/BaseListenerChainTest.kt
+++ src/test/kotlin/org/amfibot/discord/bot/BaseListenerChainTest.kt
@@ -1,6 +1,7 @@
 package org.amfibot.discord.bot
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class BaseListenerChainTest {
@@ -8,19 +9,19 @@ class BaseListenerChainTest {
     @Test
     fun iteratesChainCorrectly() {
         val buildExpected = BooleanArray(3) { true }
-        val expected = buildExpected.plus(BooleanArray(2) {false})
-        val actual = BooleanArray(5) {false}
+        val expected = buildExpected.plus(BooleanArray(2) { false })
+        val actual = BooleanArray(5) { false }
 
         val chain: Collection<EventListener> = listOf(
-            EventListener { actual[0] = true; false },
-            EventListener { actual[1] = true; false },
-            EventListener { actual[2] = true; true },
-            EventListener { actual[3] = true; false },
-            EventListener { actual[4] = true; false },
+            EventListener { _, _ -> actual[0] = true; false },
+            EventListener { _, _ -> actual[1] = true; false },
+            EventListener { _, _ -> actual[2] = true; true },
+            EventListener { _, _ -> actual[3] = true; false },
+            EventListener { _, _ -> actual[4] = true; false },
         )
 
         val listenerChain = BaseListenerChain(chain)
-        assertEquals(listenerChain.processEvent(emptyEvent), true)
+        assertEquals(listenerChain.processEvent(emptyEvent, null), true)
 
 
         assertArrayEquals(expected, actual)
